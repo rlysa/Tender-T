f6dcd214779bab7d99da459509b152d823e6095d
rlysa <r1757r@gmail.com>
2025-09-24 16:45:15 +0300

    parsing zakupki.gov: add sort with AI  file names (DON'T WORK AFTER FILTERS)

--- parse_zakupki.py
+++ parse_zakupki.py
@@ -1,40 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import date, datetime
 
-from config import ZAKUPKI_TOKEN, AI_URL, AI_API_KEY
+from datetime import date, datetime
+import tiktoken
+
+from config import *
 from prompts import *
 
 
 def get_text_from_file(file_name='ПРАЙС РДК ИЮЛЬ ОПТ СО СКИДКОЙ.pdf'):
     try:
         extension = file_name.split('.')[-1].lower()
+        file_text = ''
         if extension == 'pdf':
             import PyPDF2
-            file_text = ''
 
-            file_name = file_name
             with open(file_name, 'rb') as file:
                 reader = PyPDF2.PdfReader(file)
                 for i, page in enumerate(reader.pages):
                     file_text += page.extract_text() + '\n'
+
             return file_text
-        # elif extension == 'xlsx':
-            # import openpyxl
-            #
-            # workbook = openpyxl.load_workbook(file_name)
-            # first_sheet = workbook.get_sheet_names()[0]
-            # worksheet = workbook.get_sheet_by_name(first_sheet)
 
-            # for row in worksheet.iter_rows():
-            #     print(row)
-            #     for cell in row:
-            #         print(cell)
+        elif extension == 'xlsx':
+            from openpyxl import load_workbook
+            from openpyxl.utils import get_column_letter
+            from openpyxl.styles.numbers import BUILTIN_FORMATS
 
-            # import pandas as pd
-            # pd.set_option('display.max_columns', None)
-            # file_text = pd.read_excel(file_name)
-            # print(file_text)
+            wb = load_workbook(file_name, data_only=True)
+            sheet = wb.active
+            for row in sheet.iter_rows():
+                for cell in row:
+                    if cell.value is not None and cell.value != 0:
+                        if isinstance(cell.value, (int, float)):
+                            fmt = cell.number_format
+                            try:
+                                file_text += str(cell.value)  if fmt == 'General' else format(cell.value, '.2f') + ' '
+                            except:
+                                file_text += str(cell.value) + ' '
+                        else:
+                            file_text += str(cell.value) + ' '
+                file_text += '\n'
+        while '\n\n' in file_text:
+            file_text = file_text.replace('\n\n', '\n')
+        return file_text
     except Exception as e:
         print(f'Ошибка при чтении файла\n{e}')
 
@@ -77,7 +86,7 @@ def get_url(start_date, end_date):
     return url
 
 
-def make_request_to_ai(prompt, text, model='Qwen/Qwen2.5-Coder-32B-Instruct'):
+def make_request_to_ai(text, model=MODEL):
     try:
         headers = {
             'Authorization': f'Bearer {AI_API_KEY}',
@@ -85,7 +94,7 @@ def make_request_to_ai(prompt, text, model='Qwen/Qwen2.5-Coder-32B-Instruct'):
         }
         data = {
             'model': model,
-            'messages': [{'role': 'user', 'content': prompt + text}, ]
+            'messages': [{'role': 'user', 'content': text}, ]
         }
 
         response = requests.post(AI_URL, headers=headers, json=data)
@@ -105,10 +114,42 @@ def save_result(file_name, *result):
         print(f'Ошибка при сохранении результата в файл\n{e}')
 
 
+def less_max_tokens(prompt, full_text, model=MODEL):
+    def count_tokens(text):
+        if 'gpt' in model:
+            enc = tiktoken.encoding_for_model(model)
+            tokens = enc.encode(prompt + text)
+        elif 'Qwen' in model:
+            try:
+                from transformers import AutoTokenizer
+                tokenizer  = AutoTokenizer.from_pretrained(model)
+                tokens = tokenizer.encode(prompt + text)
+            except Exception as e:
+                print(f'Ошибка при подсчете токенов\n{e}')
+        return tokens
+
+    tokens_full_text = len(count_tokens(full_text))
+    tokens_prompt = len(count_tokens(prompt))
+    print(f'Количество токенов: {tokens_prompt + tokens_full_text}')
+    if tokens_full_text + tokens_prompt > MAX_TOKENS:
+        max_tokens_text = (MAX_TOKENS - tokens_prompt)
+        count = tokens_full_text // max_tokens_text + 1
+        full_text = [prompt + full_text[i * max_tokens_text:i * max_tokens_text + MAX_TOKENS] for i in range(0, count)]
+    else:
+        full_text = [prompt + full_text]
+    return full_text
+
+
 def main():
     # key_words = get_key_words()[:7]
-    file_text = get_text_from_file()
-    key_words_answ = make_request_to_ai(promt_get_key_words, file_text)
+    file_text = get_text_from_file('ПРАЙС РДК ИЮЛЬ ОПТ СО СКИДКОЙ.xlsx')
+    tokens_file_text = less_max_tokens(promt_get_key_words, file_text, MODEL)
+
+    key_words_answ = []
+    for i in tokens_file_text:
+        key_words_answ.append(make_request_to_ai(i))
+        print(f'{datetime.now()}: Обработка файла: {len(key_words_answ)}/{len(tokens_file_text)}')
+    key_words_answ = '\n'.join(key_words_answ)
     # save_result('key_words_ai.txt', key_words_answ)
     key_words = key_words_answ.split('\n')
     print(f'{datetime.now()}: Выделены ключевые слова')
@@ -120,8 +161,9 @@ def main():
                'Content-Type': 'application/json'}
 
     cards = {}
+    docs = {}
     for word in key_words[0:7]:
-        url_word = url.replace('searchString=&', f'searchString={word}&')
+        url_word = url.replace('searchString=&', f'searchString={word.strip()}&')
         response_word = requests.get(url_word, headers=headers)
         soup_word = BeautifulSoup(response_word.text, 'html.parser')
         blocks = soup_word.find_all('div', class_='row no-gutters registry-entry__form mr-0')
@@ -131,28 +173,44 @@ def main():
             name = i.find('div', class_='registry-entry__body-value').get_text().strip()
             cost = i.find('div', class_='price-block__value').get_text().strip()
             link_on_docs = f'https://zakupki.gov.ru{i.find('div', class_='href d-flex').find('a').get('href')}'
-            if 'noticeInfoId' in link_on_docs:
-                response_docs = requests.get(link_on_docs, headers=headers)
-                soup_docs = BeautifulSoup(response_docs.text, 'html.parser')
-                docs_names_block = soup_docs.find('div', class_='row pl-3').find_all('span', class_='count')
-                docs_names = [['https://zakupki.gov.ru' + j.find_all('a')[1].get('href'),  j.find_all('a')[1].get_text().strip()] for j in docs_names_block]
-                print('-----------------------------------\n', docs_names)
+            response_docs = requests.get(link_on_docs, headers=headers)
+            soup_docs = BeautifulSoup(response_docs.text, 'html.parser')
             cards[number] = [name, cost, link_on_docs]
+            try:
+                if 'noticeInfoId' in link_on_docs:
+                    docs_names_block = soup_docs.find('div', class_='row pl-3').find_all('span', class_='count')
+                    docs_names = [['https://zakupki.gov.ru' + j.find_all('a')[1].get('href'),  j.find_all('a')[1].get_text().strip()] for j in docs_names_block]
+                elif 'regNumber' in link_on_docs:
+                    docs_names_block = soup_docs.find('div', class_='blockFilesTabDocs').find_all('span', class_='section__value')
+                    docs_names = [['https://zakupki.gov.ru' + j.find('a').get('href'), j.find('a').get_text().strip()] for j in docs_names_block]
+                for i in docs_names:
+                    docs[i[1]] = i[0]
+                    cards[number].append(i[1])
+            except Exception as e:
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', link_on_docs, e, response_docs.text)
+                cards[number].append(' ')
 
     print(f'{datetime.now()}: Собраны все карточки.')
     save_result('key_words_cards.txt', key_words_answ, '\n'.join([cards[i][2] for i in cards]))
 
+    cards_info = '\n'.join([f'{i}: {cards[i][0]} Документы: {', '.join(cards[i][3:])}' for i in cards])
+    tokens_cards = less_max_tokens(promt_get_cards.replace('//Заменить1//', key_words_answ.replace('\n', ', ')), cards_info)
+    true_cards_answ = []
+    for i in tokens_cards:
+        true_cards_answ.append(make_request_to_ai(i))
+        print(f'{datetime.now()}: Обработка файла: {len(true_cards_answ)}/{len(tokens_cards)}')
+
+    true_cards_answ = '\n'.join(true_cards_answ).split('\n')
     print(cards)
-    cards_info = '\n'.join([f'{i}: {cards[i][0]}' for i in cards])
-    true_cards_pr = promt_get_cards.replace('//Заменить1//', key_words_answ.replace('\n', ', ')).replace('//Заменить2//', cards_info)
     true_cards = {}
-
-    true_cards_answ =make_request_to_ai(true_cards_pr, '').split(',')
-    print(true_cards_answ)
     for i in true_cards_answ:
-        true_cards[i.strip()] = cards[i.strip()]
+        card_num, doc_name = i.strip().split(':')
+        doc = requests.get(docs[doc_name.strip()], headers=headers)
+        true_cards[card_num.strip()] = cards[card_num.strip()][:3] + [doc_name.strip()]
     print(f'{datetime.now()}: Отобраны релевантные карточки.')
+    print(true_cards)
 
+    return
     # for card in true_cards:
     #     response_docs = requests.get(true_cards[card][2], headers=headers)
     #     soup_docs = BeautifulSoup(response_docs.text, 'html.parser')
@@ -170,3 +228,4 @@ def main():
 if __name__ == '__main__':
     main()
     # get_text_from_file('ПРАЙС РДК ИЮЛЬ ОПТ СО СКИДКОЙ.xlsx')
+    # print(make_request_to_ai(f'Почему токенов в первом случае больше чем во втором, покажи различия в тексте. В первом случае excel файл парсился с помощью openpyxl, во втором случае сначала преобразован в pdf, потом парсился с помощью pypdf2. Первый случай: {get_text_from_file('ПРАЙС РДК ИЮЛЬ ОПТ СО СКИДКОЙ.xlsx')} Второй случай: {get_text_from_file()}'))
