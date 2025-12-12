70aa878f65023ff4017f9b2ef141804472c93f4b
rlysa <r1757r@gmail.com>
2025-12-12 17:50:57 +0300

    chto-to 2

--- bot/__routers.py
+++ bot/__routers.py
@@ -2,6 +2,7 @@ from bot.start import router as router_start
 from bot.add_script import router as router_add_script
 from bot.get_access import router as router_get_access
 from bot.admin import router as router_admin
+from bot.pipeline_add_script import router as router_add_script_P
 
 
-routers = [router_start, router_add_script, router_get_access, router_admin]
+routers = [router_start, router_add_script, router_get_access, router_admin, router_add_script_P]
